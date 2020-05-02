d954ec6e031ba7d8dac9fa22fcdd9cf1abdd8718
Imraan Ali <imraan91@hotmail.co.uk>
2020-05-02 20:35:41 +0100

    remove field with errors functionality to stop indentation being changed

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#remove the field with errors functionality that changes indentation 
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
